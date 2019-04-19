1d293b13d2ab507f9f13b25544800038ce78e40e
thailinh <linhcanthai@vccorp.vn>
2019-04-19 17:59:38 +0700

    Initial Commit

---
+++ MacOs_JsonToDataBase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOs_JsonToDataBase
+//
+//  Created by thailinh on 4/19/19.
+//  Copyright © 2019 thailinh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
